b2aaa63057c40bff47fb0ab5015400d490b0a2b3
jsamar17 <jsamar@google.com>
2018-08-29 17:27:48 -0700

    refactor: Implemented squashDetections() which compresses the detections in a PackageTree

--- src/output-util.ts
+++ src/output-util.ts
@@ -13,3 +13,29 @@ export function getNumberOfTransitiveDetections(
   });
   return totalDependencies;
 }
+
+export function squashDetections(packageTree: PackageTree<PointOfInterest[]>):
+    Map<string, number> {
+  const sortedDataArray = packageTree.data.sort(compare);
+  const squashedDetections: Map<string, number> = new Map();
+  for (const point of sortedDataArray) {
+    if (squashedDetections.has(point.type)) {
+      squashedDetections.set(
+          point.type, squashedDetections.get(point.type)! + 1);
+    } else {
+      squashedDetections.set(point.type, 1);
+    }
+  }
+  return squashedDetections;
+}
+
+// Comparison function to sort Points of Interest by type
+function compare(a: PointOfInterest, b: PointOfInterest): number {
+  if (a.type < b.type) {
+    return -1;
+  }
+  if (a.type > b.type) {
+    return 1;
+  }
+  return 0;
+}
